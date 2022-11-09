300d8d236ebb88d4fecb1eac7a7d7c37063f7bbb
Florian Ranze <florian.ranze@gmail.com>
2022-11-09 19:10:06 +0100

    solution added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const button = document.querySelector("button");
-const list = document.querySelector("ul");
+const table = document.querySelector("table");
 
 const URL_API =
-  "https://raw.githubusercontent.com/giacomotolari/json-fetch/main/data.json";
+  "https://raw.githubusercontent.com/FbW-D04-A/fetch-from-github-Lorp91/main/data.json";
 
 // button.addEventListener("click", () => {
 //   fetch(URL_API)
@@ -16,10 +16,13 @@ const URL_API =
 
 button.addEventListener("click", async () => {
   try {
+    table.innerHTML = ``;
     const response = await fetch(URL_API);
     const data = await response.json();
     data.map((person) => {
-      return (list.innerHTML += `<li>${person.firstName} - ${person.lastName} -${person.age}</li>`);
+      let tr = document.createElement("tr");
+      table.appendChild(tr);
+      return (tr.innerHTML += `<td>${person.name}</td><td>${person.nachname}</td><td>${person.age}</td>`);
     });
   } catch (err) {
     console.log(err);
